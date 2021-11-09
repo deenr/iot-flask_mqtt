bd4dcea510d114fc8d3d384b4e53aa5f358b5850
Vincent Claes <claesvincent@gmail.com>
2021-11-09 16:19:50 +0100

    First upload

---
+++ App.py
@@ -0,0 +1,68 @@
+import eventlet
+import json
+from flask import Flask, render_template
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
+import ssl
+
+eventlet.monkey_patch()
+
+app = Flask(__name__)
+app.config['SECRET'] = 'my secret key'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_BROKER_URL'] = '5ffddf1dac9d40e2949c6440eaa9eb79.s1.eu.hivemq.cloud'
+#app.config['MQTT_BROKER_URL'] = '192.168.1.5'
+#app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'uhasselt'
+app.config['MQTT_PASSWORD'] = 'uHasselt007'
+app.config['MQTT_KEEPALIVE'] = 5
+
+# Parameters for SSL enabled
+app.config['MQTT_BROKER_PORT'] = 8883
+app.config['MQTT_TLS_ENABLED'] = True
+app.config['MQTT_TLS_INSECURE'] = False
+#app.config['MQTT_TLS_CA_CERTS'] = 'server.pem'
+app.config['MQTT_TLS_CA_CERTS'] = 'cacert.pem'
+#app.config['MQTT_TLS_CERTFILE'] = 'client.crt'
+#app.config['MQTT_TLS_KEYFILE'] = 'client.key'
+app.config['CLIENT_ID'] = 'Rocky'
+
+app.config['MQTT_TLS_VERSION'] = ssl.PROTOCOL_TLSv1_2
+
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'])
+
+@socketio.on('subscribe')
+def handle_subscribe(json_str):
+    data = json.loads(json_str)
+    mqtt.subscribe(data['topic'])
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    socketio.emit('mqtt_message', data=data)
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
