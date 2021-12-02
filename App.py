ff25f3dc4dcc1ab6dea2551dc9484fb30869d429
deenr <deenrayman@outlook.be>
2021-12-02 23:37:23 +0100

    test

--- App.py
+++ App.py
@@ -1,11 +1,12 @@
-import eventlet
 import json
+import ssl
+
+import eventlet
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_cors import CORS
 from flask_mqtt import Mqtt
 from flask_socketio import SocketIO
-from flask_bootstrap import Bootstrap
-from flask_cors import CORS, cross_origin
-import ssl
 
 eventlet.monkey_patch()
 
@@ -13,8 +14,8 @@ app = Flask(__name__)
 app.config['SECRET'] = 'my secret key'
 app.config['TEMPLATES_AUTO_RELOAD'] = False
 app.config['MQTT_BROKER_URL'] = 'e8bf9123a02b43718ff5a85861b6d572.s1.eu.hivemq.cloud'
-#app.config['MQTT_BROKER_URL'] = '192.168.1.5'
-#app.config['MQTT_BROKER_PORT'] = 1883
+# app.config['MQTT_BROKER_URL'] = '192.168.1.5'
+# app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = 'deenr'
 app.config['MQTT_PASSWORD'] = 'deenR123'
 app.config['MQTT_KEEPALIVE'] = 5
@@ -30,33 +31,40 @@ app.config['MQTT_TLS_VERSION'] = ssl.PROTOCOL_TLSv1_2
 
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 mqtt = Mqtt(app)
-#socketio = SocketIO(app)
+# socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
-socketio = SocketIO(app,cors_allowed_origins='*')
-#socketio.init_app(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+# socketio.init_app(app, cors_allowed_origins="*")
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/beerpong')
 def beerpong():
     return render_template('beerpong.html')
+
 
 @socketio.on('publish')
 def handle_publish(json_str):
     data = json.loads(json_str)
     mqtt.publish(data['topic'], data['message'])
 
+
 @socketio.on('subscribe')
 def handle_subscribe(json_str):
     data = json.loads(json_str)
     mqtt.subscribe(data['topic'])
 
+
 @socketio.on('unsubscribe_all')
 def handle_unsubscribe_all():
     mqtt.unsubscribe_all()
+
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
@@ -66,9 +74,13 @@ def handle_mqtt_message(client, userdata, message):
     )
     socketio.emit('mqtt_message', data=data)
 
+
 @mqtt.on_log()
 def handle_logging(client, userdata, level, buf):
     print(level, buf)
 
+
 if __name__ == '__main__':
+    mqtt.subscribe('subscribe_data_player_1')
+    mqtt.subscribe('subscribe_data_player_2')
     socketio.run(app, host='0.0.0.0', port=80, use_reloader=False, debug=True)
---
+++ static/js/beerpong.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    //var socket = io.connect('http://' + document.domain + ':' + location.port, {secure: true});
+	var socket = io.connect('/');
+
+    socket.on('mqtt_message', function(data) {
+        console.log(data["topic"])
+        if (data["topic"] === "subscribe_data_player_1") {
+            var payload = data["payload"];
+            sensor_values = payload.split("");
+
+            sensor_values.forEach((function (sensor_value, i) {
+                var dot = '#player1_dot' + (i+1);
+                console.log(dot)
+                if (sensor_value === "1") {
+                    $(dot).css("background-color","green");
+                } else {
+                    $(dot).css("background-color","red");
+                }
+            }));
+        }
+            
+        if (data["topic"] === "subscribe_data_player_2") {
+            var payload = data["payload"];
+            sensor_values = payload.split("");
+
+            sensor_values.forEach((function (sensor_value, i) {
+                var dot = '#player2_dot' + (i+1);
+                console.log(dot)
+                if (sensor_value === "1") {
+                    $(dot).css("background-color","green");
+                } else {
+                    $(dot).css("background-color","red");
+                }
+            }));
+        }
+    })
+  });
